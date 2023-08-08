99f6d397a09a2aafcde523efb18247e1be66313b
Claudio Matos <claudiomatos@MacBook-Air-de-Claudio.local>
2023-08-08 20:46:38 -0300

    Alterando a cor de fundo

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'grey',
+    backgroundColor: 'red',
   },
   text: {
     fontSize: 18,
